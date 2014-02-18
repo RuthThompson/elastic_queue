e9addfe5202e881d2e00770afd944f08217daa4e
Ruth Thompson <ruth@flywheelnetworks.com>
2014-02-18 14:45:29 -0800

    cleanup

--- lib/elastic_queue/railtie.rb
+++ lib/elastic_queue/railtie.rb
@@ -2,8 +2,8 @@ module ElasticQueue
   class Railtie < ::Rails::Railtie
     initializer 'ElasticQueue options initializer', after: :after_initialize do
       elastic_queue_default_options = { elasticsearch_hosts: [{ host: 'localhost', port: 9200, protocol: 'http' }] }.with_indifferent_access
-      if defined?(Rails) && File.exists?("#{Rails.root.to_s}/config/elastic_queue.yml")
-        ElasticQueue::OPTIONS = elastic_queue_default_options.merge(YAML.load_file("#{Rails.root.to_s}/config/elastic_queue.yml")[Rails.env].with_indifferent_access)
+      if defined?(Rails) && File.exists?(Rails.root.join('config', 'elastic_queue.yml').to_s)
+        ElasticQueue::OPTIONS = elastic_queue_default_options.merge(YAML.load_file(Rails.root.join('config', 'elastic_queue.yml').to_s)[Rails.env].with_indifferent_access)
       else
         ElasticQueue::OPTIONS = elastic_queue_default_options
       end
