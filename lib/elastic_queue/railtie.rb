9050c713ecbf0e651bc215d09bd96e6c9897eb2c
Ruth Thompson <ruth@flywheelnetworks.com>
2014-02-19 08:18:03 -0800

    fix problem with failure on incomplete config file, bump version

--- lib/elastic_queue/railtie.rb
+++ lib/elastic_queue/railtie.rb
@@ -1,12 +1,14 @@
 module ElasticQueue
   class Railtie < ::Rails::Railtie
     initializer 'ElasticQueue options initializer', after: :after_initialize do
-      elastic_queue_default_options = { elasticsearch_hosts: [{ host: 'localhost', port: 9200, protocol: 'http' }] }.with_indifferent_access
+      default_config_options = { elasticsearch_hosts: [{ host: 'localhost', port: 9200, protocol: 'http' }] }.with_indifferent_access
       if defined?(Rails) && File.exists?(Rails.root.join('config', 'elastic_queue.yml').to_s)
-        ElasticQueue::OPTIONS = elastic_queue_default_options.merge(YAML.load_file(Rails.root.join('config', 'elastic_queue.yml').to_s)[Rails.env].with_indifferent_access)
-      else
-        ElasticQueue::OPTIONS = elastic_queue_default_options
+        config_options = YAML.load_file(Rails.root.join('config', 'elastic_queue.yml').to_s)[Rails.env]
+        unless config_options.nil?
+          ElasticQueue::OPTIONS = default_config_options.merge(config_options.with_indifferent_access)
+        end
       end
+        ElasticQueue::OPTIONS = default_config_options unless defined? ElasticQueue::OPTIONS
     end
   end
 end
